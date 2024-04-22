e6292432de69eada4c6dbbd8db12f92f9ffa3e7b
Stina Persson <stpe1901@student.miun.se>
2024-04-22 15:53:08 +0200

    Anslutning till databsen klar

--- server.js
+++ server.js
@@ -7,35 +7,39 @@ const mongoose = require("mongoose");
 const app = express(); 
 const port = 3000; 
 
-app.use(experss.json()); 
+app.use(express.json()); 
 app.use(cors());
 
 //Anlutning till databas (MongoDB)
-
+mongoose.connect("mongodb://localhost:27017/cv").then(() => {
+    console.log("Ansluten till databasen!")
+}).catch((error)=>{
+    console.log("Fel vid anslutning till databas" + error); 
+});
 
 //Routing
 app.get("/api", (req, res)=>{
     res.json({message: "Välkommen till mitt API!"}); 
-})
+});
 
 app.get("/api/workexp", (req, res)=>{
     res.json({message: "Här finns alla poster i databasen!"})
-}) 
+}); 
 
-app.post("/api/workexp", (req, req)=>{
+app.post("/api/workexp", (req, res)=>{
     res.json({message:"Lagt till post i API"})
-}) 
+}); 
 
 app.put("/api/workexp/:id", (req, res)=> {
     res.json({message:"Uppdaterat post med id:" + req.params.id}); 
-}) 
+}); 
 
 app.delete("/api/workexp/:id", (req, res)=> {
     res.json({message:"Tagit bort post med id:" + req.params.id}); 
-})
+}); 
 
 
 //server start
 app.listen(port, ()=>{
     console.log("Servern är uppe på port:" + port); 
-}) 
+}); 
